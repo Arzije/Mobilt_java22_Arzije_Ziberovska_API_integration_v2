ca7c1d53d8e2bec4a37f52cce630d125b05bd1f9
Arzije Ziberovska <arzije.ziberovska@gmail.com>
2023-09-14 19:28:30 +0200

    Final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("com.android.volley:volley:1.2.1")
-    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation(files("libs\\json-simple-1.1.1.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
